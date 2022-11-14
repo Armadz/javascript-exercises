0195ba78252f293d54565cf916b217fddbf19620
Armadz <sehoban@gmail.com>
2022-11-14 10:24:32 -0600

    reverseString.js function implemented and tested

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(reverseString) {
+    output = '';
+    for(let i = reverseString.length; i > 0; i--){
+        output += reverseString.slice(i-1,i);
+    };
 
+    return output;
 };
 
 // Do not edit below this line
