b26fba3846160bdfb9a14d4f60a0ee3717ef8a35
Armadz <sehoban@gmail.com>
2022-11-15 13:15:50 -0600

    Complete sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(...params) {
+    let secondNum;
+    let firstNum;
 
+    for(const i in params) {
+        if (typeof params[i]!='number' || params[i] < 0) {
+            return 'ERROR'
+        };
+    };
+
+    if (params[0] > params[1]) {
+        secondNum = params[0];
+        firstNum = params[1];
+    } else {
+        firstNum = params[0];
+        secondNum = params[1];
+        };
+
+    return ((secondNum - (firstNum -1))/2)*(firstNum + secondNum);
 };
 
 // Do not edit below this line
