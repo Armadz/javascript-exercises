ab19cb817b8ccdd0009454e4c2aa346b04492775
Armadz <sehoban@gmail.com>
2022-11-16 20:36:37 -0600

    Adds functions to calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let output = 0
+	for(num in arr) {
+    output+=arr[num];
+  };
+  return output
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let output = 1
+	for(num in arr) {
+    output*=arr[num];
+  };
+  return output
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	let output = 1;
+  for(let i =0; i < b; i++) {
+    output*=a;
+  };
+  return output;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let output = 1;
+	for(let i = 1; i <= a; i++) {
+    output*=i;
+  };
+  return output;
 };
 
 // Do not edit below this line
